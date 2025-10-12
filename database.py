0ab08e2f25e4f6d00f6ac1471b40ba1081cf70b3
pranab-sarkar <pranabrobotics@gmail.com>
2025-10-12 14:49:49 +0600

    update dockerfile, database

--- database.py
+++ database.py
@@ -4,6 +4,7 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
+
 DATABASE_URL = os.environ.get("DATABASE_URL", "postgresql://postgres@localhost:5432/service_manager")
 engine = create_engine(DATABASE_URL, echo=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
